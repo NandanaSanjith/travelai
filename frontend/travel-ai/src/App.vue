0cf5265fa249a171d4240aa399ef28888508ef95
nivedithaks05 <nivedithaakaladi@gmail.com>
2025-09-19 21:27:34 +0530

    sample ui generated

--- frontend/travel-ai/src/App.vue
+++ frontend/travel-ai/src/App.vue
@@ -1,30 +1,11 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+<v-text-field label="Label" width="400"></v-text-field>
+<v-card max-width="600" min-width="300" class="pa-4">
+  <v-card-title>Responsive Width</v-card-title>
+</v-card>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+ </template>
