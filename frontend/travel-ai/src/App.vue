9fcf6678ff476c7c6df56f6944b9f4557bf2633b
Karthika896 <sskarthika896@gmail.com>
2025-09-27 20:03:25 +0530

    added appbar

--- frontend/travel-ai/src/App.vue
+++ frontend/travel-ai/src/App.vue
@@ -5,7 +5,24 @@ import SearchBar from './components/SearchBar.vue';
 
 <template>
   <v-app >
-    <search-bar></search-bar>
+    <v-app-bar :elevation="2">
+      <v-btn 
+          height="120"
+          width="160"
+          class="d-flex align-center justify-center" 
+        >
+          <v-img
+            src="src/assets/wanderwise-icon.svg"
+            height="120"
+            width="160"
+          />
+        </v-btn>
+    </v-app-bar>
+    
+    <v-main>
+      <search-bar></search-bar>
+    </v-main>
+    
     
     
   </v-app>
