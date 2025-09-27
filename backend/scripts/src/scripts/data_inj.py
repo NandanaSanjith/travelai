172c7ffc7fa63b3326789b96e1e5c8c900358911
NandanaSanjith <nandanasanjith2175@gmail.com>
2025-09-27 17:51:48 +0530

    reading data

--- backend/scripts/src/scripts/data_inj.py
+++ backend/scripts/src/scripts/data_inj.py
@@ -9,11 +9,18 @@ def ingest(source,destination,date):
     flights=get_flights(src_city=source,dest_city=destination,start_date=date)
     collection.insert_many(flights)
 
-    
+def read_data():
+    client = MongoClient("mongodb://localhost:27017/")
+    db = client["travel_ai"]
+    flights=db.flight_details.find({ "arrival_city":"BANGALORE","departure_city": "KOCHI" ,"departure_date":"2025-11-20"})
+    for flight in flights:
+        print(flight)
+
+
 if __name__ == "__main__":
     print("injest")
-    ingest(source='kochi',destination='banglore',date='2025-10-29')
-
+    ingest(source='kochi',destination='bangalore',date='2025-09-29')
+    #read_data()
        
 
 
