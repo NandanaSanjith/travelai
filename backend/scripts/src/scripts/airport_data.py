daee8d1ebdfd4abb85514e06056fb645633ae5c6
meghasudheer884 <meghasudheer884@gmail.com>
2025-10-08 19:03:12 +0530

    updated validation

--- backend/scripts/src/scripts/airport_data.py
+++ backend/scripts/src/scripts/airport_data.py
@@ -3,14 +3,16 @@ import pandas as pd
 
 from pymongo import MongoClient
 
+def is_none(value):
+  return value==None or pd.isna(value)
+
 def injest_airport_data():
  url = "https://ourairports.com/data/airports.csv"
  df = pd.read_csv(url)
 
- indian_airports = df[(df["iso_country"] == "IN") & (df["iata_code"]!= None)][["name", "municipality", "iata_code", "type"]]
+ indian_airports = df[(df["iso_country"] == "IN") & (is_none(df["iata_code"]))][["name", "municipality", "iata_code", "type"]]
  records = indian_airports.to_dict(orient="records")
- print(records)
- return 
+ return
  client = MongoClient("mongodb://localhost:27017/")  # or your connection URI
  db = client["travel_ai"]
  collection = db["airports"]
